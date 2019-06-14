da5f5ff5d5dd03a1098863c6dd4a7fb3fdde653f
k9606 <bishengfei@126.com>
2019-06-14 19:07:08 +0800

    v1 版本完成

--- md.py
+++ md.py
@@ -1,21 +1,31 @@
 from tkinter import *
 
 
-class App:
+class Md:
     def __init__(self, master):
-        frame = Frame(master)  # container
+        frame = Frame(master)
+
         frame.pack()
         self.quitButton = Text()
         self.quitButton.pack()
+        self.aa = Text()
+        self.aa.pack()
 
-        self.hiButton = Button(frame, text='Say Hi', command=self.read_file)
+        self.hiButton = Button(frame, text='转换文档', command=self.read_file)
         self.hiButton.pack()
 
     def hello(self):
         print('hello tkinter')
 
     def read_file(self):
-        with open(self.quitButton.get(0.0, "end"), encoding="utf-8") as f:
+
+        f = open('table_ddl.txt', 'w', encoding="utf-8")
+        # 将文本写入到文件
+        f.write(self.quitButton.get(0.0, "end"))
+        # 关闭文件
+        f.close()
+
+        with open('table_ddl.txt', encoding="utf-8") as f:
             txt = f.readlines()
             self.handle_file(txt)
 
@@ -28,7 +38,9 @@ class App:
                 tmp += '|' + t.strip()[1:t.strip().find('`', 1)] + '|' + self.check_type(
                     t.strip()) + '|' + self.check_comment(
                     t.strip()) + '|' + '\r\n'
+
         print(tmp, end='')
+        self.aa.insert(INSERT, tmp)
 
     def check_type(self, string):
         if string.find('int(') != -1 or string.find('tinyint(') != -1:
@@ -40,13 +52,12 @@ class App:
 
     def check_comment(self, string):
         if len(string.split("COMMENT '")) == 2:
-           // return string.split("COMMENT '")[1]
+            return string.split("COMMENT '")[1]
         else:
-          //  return 'x'
+            return 'x'
 
 
 root = Tk()
-
-app = App(root)
-
+root.title('k9606')
+app = Md(root)
 root.mainloop()
